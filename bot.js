0307aef357cf2e61b3339c37154801d897017ad2
R3Da2 <59657961+R3Da2@users.noreply.github.com>
2020-01-08 20:01:34 +0400

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("664494900347142187")
+setInterval(function() {
+channel.send(`#R3DBotSpamIsHere#R3DBotSpamIsHere#R3DBotSpamIsHere#R3DBotSpamIsHere#R3DBotSpamIsHere#R3DBotSpamIsHere#R3DBotSpamIsHere#R3DBotSpamIsHere#R3DBotSpamIsHere#R3DBotSpamIsHere#R3DBotSpamIsHere#R3DBotSpamIsHere#R3DBotSpamIsHere#R3DBotSpamIsHere#R3DBotSpamIsHere#R3DBotSpamIsHere#R3DBotSpamIsHere#R3DBotSpamIsHere#R3DBotSpamIsHere#R3DBotSpamIsHere#R3DBotSpamIsHere#R3DBotSpamIsHere#R3DBotSpamIsHere#R3DBotSpamIsHere#R3DBotSpamIsHere#R3DBotSpamIsHere#R3DBotSpamIsHere#R3DBotSpamIsHere#R3DBotSpamIsHere#R3DBotSpamIsHere#R3DBotSpamIsHere#R3DBotSpamIsHere#R3DBotSpamIsHere#R3DBotSpamIsHere#R3DBotSpamIsHere#R3DBotSpamIsHere#R3DBotSpamIsHere`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
